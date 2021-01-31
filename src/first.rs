e66df0e56b8182f1c9ee4a6c613fcca41aa5bebd
yngtodd <young.todd.mk@gmail.com>
2021-01-31 16:46:37 -0500

    Add tests for push and pop

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub struct List {
     head: Link,
 }
@@ -15,5 +17,56 @@ struct Node {
 impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
+    }
+
+    /// Push an element to the list
+    pub fn push(&mut self, elem: i32) {
+        let node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+
+        self.head = Link::More(node);
+    }
+
+    /// Pop the final element in the linked list
+    ///
+    /// Returns `None` if the list is empty, otherwise
+    /// returns the final node's element and set the list's 
+    /// head to the next node.
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+mod test {
+    use super::List;
+    #[test]
+    fn test_pop_empty() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn test_push_pop() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+
+        // Make sure that nothing is corrupted after final pop
+        list.push(10);
+        assert_eq!(list.pop(), Some(10));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), None);
     }
 }
