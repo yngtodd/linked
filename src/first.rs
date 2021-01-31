3c3127daa3b9e103fdc50c4ef5c86514d8f78fa2
yngtodd <young.todd.mk@gmail.com>
2021-01-31 17:45:55 -0500

    Add Drop for the linked list
    Drop won't work out of the box here in a tail-recursive manner.

--- src/first.rs
+++ src/first.rs
@@ -45,6 +45,18 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        // If the link has a next node, replace that link with an empty  link,
+        // and go to the next link. We will do this for all nodes, and the 
+        // list will finally go out of scope here.
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty)
+        }
+    }
+}
+
 mod test {
     use super::List;
     #[test]
